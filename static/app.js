1427bc869e5cfce95bc8a3595d33bd6d3fa75b6b
Gildas Chabot <gildaschbt@gmail.com>
2018-01-19 16:44:45 +0100

    js: keep the same volume when switching album

--- static/app.js
+++ static/app.js
@@ -1,10 +1,20 @@
+var ap = undefined;
+const defaultVolume = 0.5;
+
 function play(e, i) {
+    var volume = defaultVolume;
     if (ap != undefined) {
+        var volBar = document.getElementsByClassName('aplayer-volume')[0];
+        if (volBar != undefined) {
+            volume = parseInt(volBar.style.height) / 100;
+        }
         ap.destroy();
     }
-    var ap = new APlayer({
+
+    ap = new APlayer({
         element: document.getElementById('player'),
         music: albums[i]
     });
+    ap.volume(volume);
     ap.play();
 }
